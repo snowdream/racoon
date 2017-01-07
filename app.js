5fc01d984aa21097ab534fd7a572056b84836c78
snowdream <yanghui1986527@gmail.com>
2017-01-07 23:14:02 +0800

    Add: gzip and cache

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mime = require('mime');
+var compression = require('compression');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -12,6 +13,7 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
+app.use(compression()); //use compression
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -21,7 +23,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), {maxAge:'1d'}));
 
 express.static.mime.define({
     'text/plain': ['pac']
